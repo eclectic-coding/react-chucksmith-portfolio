4193dba2fc2852d5c7db22f963e383f19cf51f11
eclectic-coding <chuck@eclecticsaddlebag.com>
2020-01-26 08:31:40 -0500

    Clean up components to begin project.

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,12 +3,8 @@ import React, { Component } from 'react'
 class Home extends Component {
   render() {
     return (
-      <div className="container">
-        <h1 className="title">Bulma</h1>
-        <p className="subtitle">
-          Modern CSS framework based on{' '} <a
-          href="https://developer.mozilla.org/en-US/docs/Web/CSS/CSS_Flexible_Box_Layout/Basic_Concepts_of_Flexbox"> Flexbox </a>
-        </p>
+      <div >
+        <h2 >Hello from Home</h2>
       </div>
     )
   }
